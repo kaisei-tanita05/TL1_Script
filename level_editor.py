d4c04f61bf9b00bb401cbb5a3b2b80ae103cd05c
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2026-06-22 12:30:38 +0900

    Update level_editor.py
    TL1_01_08

--- level_editor.py
+++ level_editor.py
@@ -88,7 +88,10 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
             indent+="\t"           
                 
         # 2. 各オブジェクトの情報を書き込む
-        self.write_and_print(file, indent + object.type + " - " + object.name)
+        """self.write_and_print(file, indent + object.type + " - " + object.name)"""
+        
+        self.write_and_print(file, indent + object.type)
+
 
         trans, rot, scale = object.matrix_local.decompose()
         #回転をQuaternionからEuler (3軸)
@@ -100,10 +103,17 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         rot.z = math.degrees(rot.z)
                 
         # 画像通りの出力にするため、改行なしで連続書き込み
-        self.write_and_print(file, indent + "Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
-        self.write_and_print(file, indent + "Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
-        self.write_and_print(file, indent + "Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+        self.write_and_print(file, indent + "T (%f,%f,%f)" % (trans.x, trans.y, trans.z))
+        self.write_and_print(file, indent + "R (%f,%f,%f)" % (rot.x, rot.y, rot.z))
+        self.write_and_print(file, indent + "S (%f,%f,%f)" % (scale.x, scale.y, scale.z))
         self.write_and_print(file,'')
+
+
+        #カスタムプロパティ
+        if "file_name" in object:
+            self.write_and_print(file, indent + "N %s" % object["file_name"])
+        self.write_and_print(file, indent + 'END')
+        self.write_and_print(file, '')
 
         #子ノードへ進む(深さが1上がる)
         for child in object.children:
@@ -114,6 +124,41 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         self.export()
         self.report({'INFO'}, "シーン情報をExportしました")
         print("シーン情報をExportしました")
+        return {'FINISHED'}
+
+#パネル ファイル名
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル"""
+    bl_idname = "OBJECT_PT_file_name"
+    bl_label = "FileName"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    # サブメニューの描画
+    def draw(self, context):
+
+        #パネル項目を追加
+        if "file_name" in context.object:
+            #既にプロパティーがあれば、プロパティを表示
+            self.layout.prop(context.object, '["file_name"]', text=self.bl_label)
+        else:
+            #プロパティがなければ、プロパティ追加ボタンを表示
+            self.layout.operator(MYADDON_OT_add_filename.bl_idname)
+
+
+#オペレータ カスタムプロパティ「file_name」追加
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_filename"
+    bl_label = "FileName 追加"
+    bl_description = "['file_name']カスタムプロパティを追加します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+
+        #['file_name']カスタムプロパティを追加
+        context.object["file_name"] = ""
+
         return {'FINISHED'}
 
 # トップバーの拡張メニュー
@@ -146,7 +191,9 @@ classes = (
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
     MYADDON_OT_export_scene,
-    TOPBAR_MT_my_menu
+    TOPBAR_MT_my_menu,
+    MYADDON_OT_add_filename,
+    OBJECT_PT_file_name,
 )
 
 # Add-On有効化時コールバック
